04cc3cd9e07a89637412862da61a2d4a8c894657
Monal Narasimhamurthy <monal.narasimhamurthy@colorado.edu>
2016-11-30 16:12:04 -0700

    Add function to print graph

--- graph.ml
+++ graph.ml
@@ -8,6 +8,32 @@ type context        = (adjacency_list * node_index * node_name * adjacency_list)
 
 type graph          = Empty | Graph of (context * graph)
 
+(* Function to represent an adjancecy list as a string *)
+let rec string_of_adj_list: adjacency_list -> string =
+  fun a -> match a with 
+             | [] -> ""
+             | (label, idx)::tail -> match tail with 
+		                       | [] -> "(" ^ label ^ "," ^ string_of_int idx ^ ")"  
+				       | _ -> "(" ^ label ^ "," ^ string_of_int idx ^ ")" ^ "," ^ string_of_adj_list tail
+                                     
+
+(* Function to represent a context as a string *)
+let string_of_context: context -> string =
+  fun c -> match c with 
+             | (a, idx, name, b) -> "([" ^ string_of_adj_list a ^ "]," ^ string_of_int idx ^ "," ^ name ^ ",[" ^ string_of_adj_list b ^ "]) & "
+
+(* Function to represent a graph as a string *)
+let rec string_of_graph: graph -> string =
+  fun g -> match g with
+             | Empty -> "Empty"
+             | Graph(c, g') -> string_of_context c ^ string_of_graph g'
+
+(* Example Usage *)
+let example_print = 
+  let g = Graph(([("left", 2);("up", 3)], 1, "a", [("right", 2)]), Graph(([("right", 1)], 2, "b", [("left", 1)]), Graph(([], 1, "a", []), Empty))) in
+  let string_of_g = string_of_graph g in
+  print_string string_of_g
+
 (* Function to check if the graph is empty or not *)
 let is_empty: graph -> bool =
   fun g -> match g with
