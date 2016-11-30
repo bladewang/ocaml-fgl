2115475eef7001eef460cc36b93ec6a7904b9004
Monal Narasimhamurthy <monal.narasimhamurthy@colorado.edu>
2016-11-30 14:06:26 -0700

    Add map function for graphs, a special case of map function - a function to reverse the edges in a graph

--- graph.ml
+++ graph.ml
@@ -13,3 +13,20 @@ let is_empty: graph -> bool =
   fun g -> match g with
              | Empty -> true
              | _ -> false
+
+(* Map function for graphs *)
+let rec gmap: (context -> context) -> graph -> graph = 
+  fun f g -> match g with
+               | Empty -> Empty
+               | Graph(c, g') -> Graph(f c, gmap f g')
+
+(* Function to swap in-going and out-going adjacency lists for a graph *)
+let swap: context -> context =
+  fun c -> match c with
+             | (a, idx, name, b) -> (b, idx, name, a)
+ 
+(* Function to reverse a graph *)
+let rec grev: graph -> graph =
+  fun g -> match g with
+	     | Empty -> Empty
+             | Graph(c, g') -> Graph(swap c, grev g')
