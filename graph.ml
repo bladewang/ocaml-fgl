1fbc91f0abd0c9e0f66f01823eee5a699c55e067
Monal Narasimhamurthy <monal.narasimhamurthy@colorado.edu>
2016-12-07 15:55:59 -0700

    Add node match; Fix typo

--- graph.ml
+++ graph.ml
@@ -20,13 +20,13 @@ let rec string_of_adj_list: adjacency_list -> string =
 (* Function to represent a context as a string *)
 let string_of_context: context -> string =
   fun c -> match c with 
-             | (a, idx, name, b) -> "([" ^ string_of_adj_list a ^ "]," ^ string_of_int idx ^ "," ^ name ^ ",[" ^ string_of_adj_list b ^ "]) & "
+             | (a, idx, name, b) -> "([" ^ string_of_adj_list a ^ "]," ^ string_of_int idx ^ "," ^ name ^ ",[" ^ string_of_adj_list b ^ "])"
 
 (* Function to represent a graph as a string *)
 let rec string_of_graph: graph -> string =
   fun g -> match g with
              | Empty -> "Empty"
-             | Graph(c, g') -> string_of_context c ^ string_of_graph g'
+             | Graph(c, g') -> string_of_context c ^ " & " ^ string_of_graph g'
 
 (* Function to check if the graph is empty or not *)
 let is_empty: graph -> bool =
@@ -66,14 +66,31 @@ let rec grev: graph -> graph =
 	     | Empty -> Empty
              | Graph(c, g') -> Graph(swap c, grev g')
 
+(* Function to match a node in a graph *)
+let rec gmatch: node_index -> graph -> (context * graph) option = 
+  fun n g -> match g with
+               | Empty -> None
+               | Graph(c, g') -> match c with 
+			           |(_, m, _, _) -> 
+				      if m = n then 
+                                        Some(c, g') 
+                                      else 
+                                        (gmatch n g')
+    
+(* Function to print Graph matches *)
+let print_gmatch: (context * graph) option -> string = 
+  fun m -> match m with 
+             | None -> "no match found"
+             | Some(ctx, g') -> string_of_context ctx
+  
 (* Example Usage *)
 let example_print =
-  let g = Graph(([("left", 2);("up", 3)], 2, "c", [("right", 2)]), Graph(([("right", 1)], 2, "b", [("left", 1)]), Graph(([], 1, "a", []), Empty))) in
+  let g = Graph(([("left", 2);("up", 3)], 3, "c", [("right", 2)]), Graph(([("right", 1)], 2, "b", [("left", 1)]), Graph(([], 1, "a", []), Empty))) in
 
   let string_of_g = string_of_graph g in
   print_string ("Graph: " ^ string_of_g ^ "\n");
   print_string ("Is Empty: " ^ string_of_bool (is_empty g) ^ "\n");
   print_string ("Graph Map: " ^ string_of_graph (gmap map g) ^ "\n");
   print_string ("Graph Fold: " ^ string_of_int (ufold fold 0 g) ^ "\n");
-  print_string ("Graph Reverse: " ^ string_of_graph (grev g) ^ "\n")
- 
+  print_string ("Graph Reverse: " ^ string_of_graph (grev g) ^ "\n");
+  print_string ("Graph Match: " ^ print_gmatch (gmatch 3 g) ^ "\n") 
